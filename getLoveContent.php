309c5f3edac788beb0b5f0dfde6317c719fb9747
jxlpzqc <jxlpzqc@live.com>
2017-05-29 10:06:56 +0800

    init project in Eclipse

---
+++ getLoveContent.php
@@ -0,0 +1,28 @@
+<?php
+	//Created By An honesty liar on 2017/4/30
+	include "connection.php";
+	if (@$_GET['number'] == null)
+	{$number = 0;}
+	else{
+		$number = $_GET['number'];
+	}
+	
+	
+	$db->query("set names utf8");
+	$sth = $db->prepare("SELECT a.c_id ,a.content ,a.time
+				,b.nickname 
+				FROM `love_content` a
+				LEFT JOIN `users` b on a.u_id = b.id
+				where a.c_id > :number  and a.private_state = 0
+				limit 0,10
+					");
+	$sth->execute(array('number' => $number ));
+	$Queryresult = $sth->fetchAll(PDO::FETCH_ASSOC);
+	
+	echo json_encode($Queryresult);
+	
+	
+	
+	
+
+?>
