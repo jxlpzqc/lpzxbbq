11723cec36a6ad6f2c531a33501ee3fc6a65f166
jxlpzqc <jxlpzqc@live.com>
2017-05-29 11:25:30 +0800

    finish asynchronousLoading function

--- getLoveContent.php
+++ getLoveContent.php
@@ -1,28 +1,27 @@
 <?php
-	//Created By An honesty liar on 2017/4/30
-	include "connection.php";
-	if (@$_GET['number'] == null)
-	{$number = 0;}
-	else{
-		$number = $_GET['number'];
-	}
-	
-	
-	$db->query("set names utf8");
-	$sth = $db->prepare("SELECT a.c_id ,a.content ,a.time
-				,b.nickname 
+//Created By An honesty liar on 2017/4/30
+header("Content-type: text/html; charset=utf-8");
+
+include "connection.php";
+if (@$_GET['number'] == null)
+{$number = 0;}
+else{
+	$number = $_GET['number'];
+}
+
+
+$db->query("set names utf8");
+$sth = $db->prepare("SELECT a.c_id ,a.content ,a.time
+				,b.nickname
 				FROM `love_content` a
 				LEFT JOIN `users` b on a.u_id = b.id
 				where a.c_id > :number  and a.private_state = 0
 				limit 0,10
 					");
-	$sth->execute(array('number' => $number ));
-	$Queryresult = $sth->fetchAll(PDO::FETCH_ASSOC);
-	
-	echo json_encode($Queryresult);
-	
-	
-	
-	
+$sth->execute(array('number' => $number ));
+$Queryresult = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+
+print(json_encode($Queryresult));
 
 ?>
